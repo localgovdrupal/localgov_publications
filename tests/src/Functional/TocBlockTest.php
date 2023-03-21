df66656015ee4fe32e91de18fb03bfa0385d9d14
Rupert Jabelman <rupert@jabelman.org>
2023-03-21 14:25:01 +0000

    Changed how expected IDs in the tocBlock are tested.

--- tests/src/Functional/TocBlockTest.php
+++ tests/src/Functional/TocBlockTest.php
@@ -34,10 +34,12 @@ class TocBlockTest extends BrowserTestBase {
     yield [
       'content' => '<h2 id="heading-1">Heading 1</h2><p>Content 1.</p><h2 id="heading-2">Heading 2</h2><p>Content 2.</p>',
       'display' => TRUE,
+      'expectedIDs' => ['#heading-1', '#heading-2'],
     ];
     yield [
       'content' => '<p>Content 1.</p><p>Content 2.</p>',
       'display' => FALSE,
+      'expectedIDs' => [],
     ];
   }
 
@@ -45,7 +47,7 @@ class TocBlockTest extends BrowserTestBase {
    * Test the block displays with content.
    * @dataProvider contentProvider
    */
-  public function testTocBlockDisplays($content, $display) {
+  public function testTocBlockDisplays(string $content, bool $display, array $expectedIDs) {
 
     $this->createNode([
       'type' => 'localgov_publication_page',
@@ -58,13 +60,16 @@ class TocBlockTest extends BrowserTestBase {
       'status' => NodeInterface::PUBLISHED,
     ]);
     $this->drupalGet('/node/1');
+
     if ($display) {
       $this->assertSession()->responseContains('On this page');
-      $this->assertSession()->responseContains('#heading-1');
-      $this->assertSession()->responseContains('#heading-2');
     }
     else {
       $this->assertSession()->responseNotContains('On this page');
+    }
+
+    foreach ($expectedIDs as $expectedID) {
+      $this->assertSession()->responseContains($expectedID);
     }
   }
 }
