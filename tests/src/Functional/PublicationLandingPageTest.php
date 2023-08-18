35182e43501745ffb631ab7c110c9da08963b0b9
Justine Pocock <justine@whoopee.digital>
2023-08-18 13:31:15 +0100

    remove the other bits of paragraphs on pubs landing pages

--- tests/src/Functional/PublicationLandingPageTest.php
+++ tests/src/Functional/PublicationLandingPageTest.php
@@ -70,7 +70,6 @@ class PublicationLandingPageTest extends BrowserTestBase {
     $this->assertSession()->statusCodeEquals(200);
     $this->assertSession()->pageTextContains('body');
     $this->assertSession()->pageTextContains('localgov_documents');
-    $this->assertSession()->pageTextContains('localgov_pub_landing_content');
     $this->assertSession()->pageTextContains('localgov_published_date');
     $this->assertSession()->pageTextContains('field_localgov_services_landing');
     $this->assertSession()->pageTextContains('localgov_updated_date');
