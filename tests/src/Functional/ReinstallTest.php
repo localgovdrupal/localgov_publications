4550f99bac13cd272f12969ad53af66ed6ae6bd3
Rupert Jabelman <rupert@jabelman.org>
2024-04-15 13:26:34 +0100

    Removed the localgov_publications_heading_ids filter before trying to remove the module.

--- tests/src/Functional/ReinstallTest.php
+++ tests/src/Functional/ReinstallTest.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\Tests\localgov_publications\Functional;
 
+use Drupal\filter\Entity\FilterFormat;
+use Drupal\filter\FilterFormatInterface;
 use Drupal\Tests\BrowserTestBase;
 
 /**
@@ -45,6 +47,9 @@ class ReinstallTest extends BrowserTestBase {
     // Confirm module has been installed.
     $this->assertTrue($moduleHandler->moduleExists('localgov_publications'));
 
+    // Remove the localgov_publications_heading_ids filter.
+    $this->removeFilter();
+
     // Uninstall module.
     $moduleInstaller->uninstall(['localgov_publications']);
 
@@ -53,6 +58,27 @@ class ReinstallTest extends BrowserTestBase {
 
     // Install module.
     $this->assertTrue($moduleInstaller->install(['localgov_publications']));
+  }
+
+  /**
+   * Removes the localgov_publications_heading_ids filter.
+   *
+   * To allow localgov_publications to be uninstalled, the
+   * localgov_publications_heading_ids filter must be removed from the wysiwyg
+   * filter format. People that wish to remove this module will need
+   * to do it manually, but we'll automate it for the purposes of letting this
+   * test run.
+   */
+  protected function removeFilter(): void {
+    $wysiwygFormat = FilterFormat::load('wysiwyg');
+    $this->assertInstanceOf(FilterFormatInterface::class, $wysiwygFormat);
+
+    // We need to call this to let the next call to removeFilter() work. If you
+    // don't call this first, the object's filterCollection property isn't set.
+    $wysiwygFormat->filters('localgov_publications_heading_ids');
+
+    $wysiwygFormat->removeFilter('localgov_publications_heading_ids');
+    $wysiwygFormat->save();
   }
 
 }
