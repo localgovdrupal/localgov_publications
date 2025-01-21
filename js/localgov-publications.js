a004c85801e97fa2192410ed3990811121446852
Rupert Jabelman <rupert@jabelman.org>
2025-01-21 15:18:24 +0000

    Code style.

--- js/localgov-publications.js
+++ js/localgov-publications.js
@@ -4,16 +4,16 @@
       $(context)
         .find('.book-outline-form')
         .drupalSetSummary(() => {
-        const $select = $(context).find('.book-title-select');
-        const val = $select[0].value;
-        if (val === '0') {
-          return Drupal.t('Not in publication');
-        }
-        if (val === 'new') {
-          return Drupal.t('New publication');
-        }
-        return Drupal.checkPlain($select.find(':selected')[0].textContent);
-      });
-    }
+          const $select = $(context).find('.book-title-select');
+          const val = $select[0].value;
+          if (val === '0') {
+            return Drupal.t('Not in publication');
+          }
+          if (val === 'new') {
+            return Drupal.t('New publication');
+          }
+          return Drupal.checkPlain($select.find(':selected')[0].textContent);
+        });
+    },
   };
 })(jQuery, Drupal);
