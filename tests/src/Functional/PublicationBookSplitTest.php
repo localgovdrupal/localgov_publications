bb71aec839a5c00e888de9eb9947f159d317c1eb
Andy Broomfield <andy.broomfield@brighton-hove.gov.uk>
2025-02-17 17:57:18 +0000

    Add test for edit nodes to split books / publications
    Checks the node edit forms also split books and publications.
    Required a small amend to make sure the Create a new book is changed to
    publication on the edit page, as this can use the node number.

--- tests/src/Functional/PublicationBookSplitTest.php
+++ tests/src/Functional/PublicationBookSplitTest.php
@@ -90,7 +90,7 @@ class PublicationBookSplitTest extends BrowserTestBase {
 
     $this->assertEquals($expected, $options);
 
-    // // Create a book node and check publications are filtered.
+    // Create a book node and check publications are filtered.
     $this->drupalGet('/node/add/book');
 
     // Get the book select widget.
@@ -107,6 +107,55 @@ class PublicationBookSplitTest extends BrowserTestBase {
     ];
 
     $this->assertEquals($expected, $options);
+
+    // Test filters apply to edit pages.
+    $publication_node_2 = $this->createNode([
+      'type' => 'localgov_publication_page',
+      'title' => 'Test publication page 2',
+      'status' => NodeInterface::PUBLISHED,
+    ]);
+
+    $this->drupalGet($publication_node_2->toUrl('edit-form')->toString());
+
+    // Get the book select widget.
+    $query = $this->xpath('.//select[@name="book[bid]"]//option');
+    $options = [];
+    foreach ($query as $option) {
+      $options[$option->getAttribute('value')] = $option->getText();
+    }
+
+    $expected = [
+      0 => '- None -',
+      $publication_node_2->id() => '- Create a new publication -',
+      $publication_node->id() => 'Test publication page',
+    ];
+
+    $this->assertEquals($expected, $options);
+
+    // Book node edit page.
+    $book_node_2 = $this->createNode([
+      'type' => 'book',
+      'title' => 'Test book 2',
+      'status' => NodeInterface::PUBLISHED,
+    ]);
+
+    $this->drupalGet($book_node_2->toUrl('edit-form')->toString());
+
+    // Get the book select widget.
+    $query = $this->xpath('.//select[@name="book[bid]"]//option');
+    $options = [];
+    foreach ($query as $option) {
+      $options[$option->getAttribute('value')] = $option->getText();
+    }
+
+    $expected = [
+      0 => '- None -',
+      $book_node_2->id() => '- Create a new book -',
+      $book_node->id() => 'Test book',
+    ];
+
+    $this->assertEquals($expected, $options);
+
   }
 
 }
